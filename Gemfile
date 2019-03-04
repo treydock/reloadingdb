a181c2fa9ac506ebceb8e61ad314d5e9f6d25971
Trey Dockendorf <treydock@gmail.com>
2019-03-03 20:16:42 -0500

    Add rspec-rails and configure generator

--- Gemfile
+++ Gemfile
@@ -53,6 +53,10 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
+group :development, :test do
+  gem 'rspec-rails'
+end
+
 group :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '>= 2.15'
--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,14 @@ module Shotsdb
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :erb
+      g.test_framework  :rspec, fixture: true, views: true
+      g.integration_tool :rspec, fixture: true, views: true
+      g.stylesheets     false
+      g.javascripts     false
+    end
   end
 end
