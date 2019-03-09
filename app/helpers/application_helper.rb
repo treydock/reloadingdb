a565bb7208e09e66f6ebce9b5a3383272ae4cdfd
Trey Dockendorf <treydock@gmail.com>
2019-03-09 17:39:31 -0500

    Improve load edit show and index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,15 @@ module ApplicationHelper
       when 'alert' then "alert alert-warning"
     end
   end
+
+  def index_actions(object)
+    content_tag :div, class: 'dropdown' do
+      concat button_tag('Actions', class: 'btn btn-secondary dropdown-toggle', id: 'button', type: 'button', 'aria-haspopup' => 'true', 'aria-expanded' => 'false', data: { toggle: 'dropdown' })
+      concat(content_tag(:div, class: 'dropdown-menu', 'aria-labelledby' => 'actions-dropdown') do
+        concat link_to('Show', object, class: 'dropdown-item')
+        concat link_to('Edit', send("edit_#{object.model_name.singular_route_key}_path", object), class: 'dropdown-item')
+        concat link_to('Destroy', object, method: :delete, data: { confirm: 'Are you sure?'}, class: 'dropdown-item')
+      end)
+    end
+  end
 end
