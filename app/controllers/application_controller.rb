bee3af84e506e9b31d92090f20b7c884cfef07b7
Trey Dockendorf <treydock@gmail.com>
2019-03-10 16:03:34 -0400

    Fix devise controllers to not require pundit authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery
 
-  after_action :verify_authorized, except: :index
+  after_action :verify_authorized, except: :index, unless: :devise_controller?
   after_action :verify_policy_scoped, only: :index
 end
