cda9aede25f1ff7167ee015e92322937a7869817
Trey Dockendorf <treydock@gmail.com>
2019-03-10 09:56:10 -0400

    Add more fields to loads search Add some numeric column validations for loads and bullets

--- app/models/bullet.rb
+++ app/models/bullet.rb
@@ -3,6 +3,7 @@ class Bullet < ApplicationRecord
   belongs_to :caliber
   validates :name, presence: true, uniqueness: { scope: :caliber }
   validates :caliber, presence: true
+  validates :grain, numericality: { only_integer: true }
 
   def name_grain_caliber
     "#{name} (#{caliber.name} - #{grain}gr)"
