554f0ebc0b5da555ae3877eff44755c64bb7cc2c
Trey Dockendorf <treydock@gmail.com>
2019-03-11 11:22:45 -0400

    Add some basic model tests

--- spec/models/load_spec.rb
+++ spec/models/load_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Load, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:caliber) }
+    it { is_expected.to belong_to(:brass) }
+    it { is_expected.to belong_to(:bullet) }
+    it { is_expected.to belong_to(:powder) }
+    it { is_expected.to belong_to(:primer) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of :date }
+    it { is_expected.to validate_numericality_of(:powder_weight) }
+    it { is_expected.to validate_numericality_of(:col) }
+    it { is_expected.to validate_numericality_of(:brass_length) }
+    it { is_expected.to validate_numericality_of(:brass_uses).only_integer }
+    it { is_expected.to validate_numericality_of(:speed).only_integer }
+    it { is_expected.to validate_numericality_of(:run_out) }
+    it { is_expected.to validate_numericality_of(:rounds).only_integer }
+  end
 end
--- spec/models/primer_spec.rb
+++ spec/models/primer_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Primer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of :name }
+    it do
+      skip("PENDING: https://github.com/thoughtbot/shoulda-matchers/issues/814")
+      is_expected.to validate_uniqueness_of(:name).scoped_to(:user)
+    end
+    it 'should have unique name scoped by caliber and user' do
+      user1 = create(:user)
+      user2 = create(:user)
+      create(:primer, user: user1, name: 'test')
+      expect(build(:primer, user: user2, name: 'test')).to be_valid
+      expect(build(:primer, user: user1, name: 'test')).not_to be_valid
+    end
+  end
 end
