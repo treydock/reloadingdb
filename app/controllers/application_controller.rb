333b547cbc93742b4acc1144a1a0fc80a16d726d
Trey Dockendorf <treydock@gmail.com>
2019-03-12 09:17:44 -0400

    Rescue record not found, fixes #3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
 
   after_action :verify_authorized, except: :index, unless: :devise_controller?
   after_action :verify_policy_scoped, only: :index
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+  def record_not_found
+    flash[:error] = "Record not found"
+    redirect_to action: 'index'
+  end
 end
