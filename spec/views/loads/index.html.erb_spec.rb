b75778b4eef3ddafe12044c641ecc7efa182c046
Trey Dockendorf <treydock@gmail.com>
2019-03-12 15:56:49 -0400

    Display powder name and weight together

--- spec/views/loads/index.html.erb_spec.rb
+++ spec/views/loads/index.html.erb_spec.rb
@@ -14,13 +14,11 @@ RSpec.describe "loads/index", type: :view do
     render
     assert_select "tr>td", :text => /#{@loads[0].date}/
     assert_select "tr>td", :text => @loads[0].bullet.name_grain_caliber
-    assert_select "tr>td", :text => @loads[0].powder.name
-    assert_select "tr>td", :text => /#{@loads[0].powder_weight}/
+    assert_select "tr>td", :text => /#{@loads[0].powder.name} - #{@loads[0].powder_weight}/
     assert_select "tr>td", :text => @loads[0].primer.name
     assert_select "tr>td", :text => /#{@loads[1].date}/
     assert_select "tr>td", :text => @loads[1].bullet.name_grain_caliber
-    assert_select "tr>td", :text => @loads[1].powder.name
-    assert_select "tr>td", :text => /#{@loads[1].powder_weight}/
+    assert_select "tr>td", :text => /#{@loads[1].powder.name} - #{@loads[1].powder_weight}/
     assert_select "tr>td", :text => @loads[1].primer.name
   end
 end
