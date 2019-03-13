73bcdbaf67c05880ae352543d94585c68ca31ed8
Trey Dockendorf <treydock@gmail.com>
2019-03-13 15:55:57 -0400

    Add unit defaults

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,20 +1,40 @@
 class Unit
   include ActiveModel::Model
 
+  def self.default_temperature
+    'F'
+  end
+
   def self.temperature
     ['F','C']
+  end
+
+  def self.default_speed
+    'mph'
   end
 
   def self.speed
     ['mph','kph']
   end
 
+  def self.default_pressure
+    'inhg'
+  end
+
   def self.pressure
     ['atm','mbar','psi','inhg','mmhg']
   end
 
+  def self.default_length
+    'in'
+  end
+
   def self.length
     ['in','mm','cm']
+  end
+
+  def self.default_distance
+    'yd'
   end
 
   def self.distance
--- spec/models/unit_spec.rb
+++ spec/models/unit_spec.rb
@@ -1,7 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Unit, type: :model do
-  describe 'units' do
+  describe 'defaults' do
+    it 'should have temperature' do
+      expect(subject.class.default_temperature).to eq('F')
+    end
+    it 'should have speed' do
+      expect(subject.class.default_speed).to eq('mph')
+    end
+    it 'should have pressure' do
+      expect(subject.class.default_pressure).to eq('inhg')
+    end
+    it 'should have length' do
+      expect(subject.class.default_length).to eq('in')
+    end
+    it 'should have distance' do
+      expect(subject.class.default_distance).to eq('yd')
+    end
+  end
+  describe 'available units' do
     it 'should have temperature' do
       expect(subject.class.temperature).to eq(['F','C'])
     end
