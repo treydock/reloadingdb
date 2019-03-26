a047c669e1fc627a6481a50b49afe8822f46a00a
Trey Dockendorf <treydock@gmail.com>
2019-03-26 10:27:24 -0400

    Sort loads so newest shown first

--- app/controllers/loads_controller.rb
+++ app/controllers/loads_controller.rb
@@ -122,6 +122,6 @@ class LoadsController < ApplicationController
     end
 
     def sort_direction
-      %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
     end
 end
