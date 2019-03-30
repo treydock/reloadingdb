dce982030c75120e71492379850506c72951e759
Trey Dockendorf <treydock@gmail.com>
2019-03-30 09:11:24 -0400

    Add some inter-model links, fixes #23

--- spec/views/shooting_logs/index.html.erb_spec.rb
+++ spec/views/shooting_logs/index.html.erb_spec.rb
@@ -4,17 +4,19 @@ RSpec.describe "shooting_logs/index", type: :view do
   include LoadViewSpecHelper
   before(:each) do
     initialize_view_helpers(view)
-    create_list(:shooting_log, 2)
+    shooting_logs = create_list(:shooting_log, 2)
+    shooting_logs[0].shooting_groups << create(:shooting_group)
+    shooting_logs[1].shooting_groups << create_list(:shooting_group, 2)
     @shooting_logs = assign(:shooting_logs, ShootingLog.page(1).all)
   end
 
   it "renders a list of shooting_logs" do
     render
     assert_select "tr>td", :text => /#{@shooting_logs[0].date}/
-    assert_select "tr>td", :text => /#{@shooting_logs[0].caliber.name}/
     assert_select "tr>td", :text => /#{@shooting_logs[0].shooting_location.name}/
+    assert_select "tr>td", :text => /1 shooting groups/
     assert_select "tr>td", :text => /#{@shooting_logs[1].date}/
-    assert_select "tr>td", :text => /#{@shooting_logs[1].caliber.name}/
     assert_select "tr>td", :text => /#{@shooting_logs[1].shooting_location.name}/
+    assert_select "tr>td", :text => /2 shooting groups/
   end
 end
