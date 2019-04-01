8183f903c51c4acf6814dadc6ddf1685f7575d87
Trey Dockendorf <treydock@gmail.com>
2019-04-01 12:32:39 -0400

    Fix javascript that changes dropdowns based on selected caliber

--- app/assets/javascripts/loads.js
+++ app/assets/javascripts/loads.js
@@ -1,30 +1,30 @@
 $(document).on("turbolinks:load", function() {
-  brass = $('#load_brass_id').html();
-  bullets = $('#load_bullet_id').html();
+  var brass = $('#load_brass_id').html();
+  var bullets = $('#load_bullet_id').html();
   $('#load_caliber_id').change(function() {
     set_by_caliber(brass, bullets);
   });
-});
 
-function set_by_caliber(brass, bullets) {
-    caliber = $('#load_caliber_id :selected').text();
-    if (!caliber) {
-      $('#load_brass_id').html(brass);
-      $('#load_bullet_id').html(bullets);
-      return
-    }
-    brass_options = $(brass).filter("optgroup[label=" + caliber + "]").html();
-    bullet_options = $(bullets).filter("optgroup[label=" + caliber + "]").html();
-    if (brass_options) {
-      $('#load_brass_id').html(brass_options);
-      $('#load_brass_id').prepend("<option value='' selected='selected'></option>");
-    } else {
-      $('#load_brass_id').empty();
-    }
-    if (bullet_options) {
-      $('#load_bullet_id').html(bullet_options);
-      $('#load_bullet_id').prepend("<option value='' selected='selected'></option>");
-    } else {
-      $('#load_bullet_id').empty();
-    }
-}
+  function set_by_caliber(brass, bullets) {
+      caliber = $('#load_caliber_id :selected').text();
+      if (!caliber) {
+        $('#load_brass_id').html(brass);
+        $('#load_bullet_id').html(bullets);
+        return
+      }
+      brass_options = $(brass).filter("optgroup[label=" + caliber + "]").html();
+      bullet_options = $(bullets).filter("optgroup[label=" + caliber + "]").html();
+      if (brass_options) {
+        $('#load_brass_id').html(brass_options);
+        $('#load_brass_id').prepend("<option value='' selected='selected'></option>");
+      } else {
+        $('#load_brass_id').empty();
+      }
+      if (bullet_options) {
+        $('#load_bullet_id').html(bullet_options);
+        $('#load_bullet_id').prepend("<option value='' selected='selected'></option>");
+      } else {
+        $('#load_bullet_id').empty();
+      }
+  }
+});
--- app/assets/javascripts/shooting_groups.js
+++ app/assets/javascripts/shooting_groups.js
@@ -1,22 +1,21 @@
 $(document).on("turbolinks:load", function() {
-  load = $('#shooting_group_load_id').html();
+  var load = $('#shooting_group_load_id').html();
   $('#shooting_group_caliber_id').change(function() {
     set_by_caliber(load);
   });
 
+  function set_by_caliber(load) {
+      caliber = $('#shooting_group_caliber_id :selected').text();
+      if (!caliber) {
+        $('#shooting_group_load_id').html(load);
+        return
+      }
+      load_options = $(load).filter("optgroup[label=" + caliber + "]").html();
+      if (load_options) {
+        $('#shooting_group_load_id').html(load_options);
+        $('#shooting_group_load_id').prepend("<option value='' selected='selected'></option>");
+      } else {
+        $('#shooting_group_load_id').empty();
+      }
+  }
 });
-
-function set_by_caliber(load) {
-    caliber = $('#shooting_group_caliber_id :selected').text();
-    if (!caliber) {
-      $('#shooting_group_load_id').html(load);
-      return
-    }
-    load_options = $(load).filter("optgroup[label=" + caliber + "]").html();
-    if (load_options) {
-      $('#shooting_group_load_id').html(load_options);
-      $('#shooting_group_load_id').prepend("<option value='' selected='selected'></option>");
-    } else {
-      $('#shooting_group_load_id').empty();
-    }
-}
