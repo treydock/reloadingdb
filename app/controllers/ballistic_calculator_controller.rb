d896d98f321ff678df06a55bbaba0d4d5cacb004
Trey Dockendorf <treydock@gmail.com>
2019-04-04 16:55:26 -0400

    Really rough idea of having ballistic data use modal popup, part of #19

--- app/controllers/ballistic_calculator_controller.rb
+++ app/controllers/ballistic_calculator_controller.rb
@@ -1,33 +1,38 @@
 class BallisticCalculatorController < ApplicationController
-  # GET /ballistic_calculators/new
+  before_action :set_associations
+
+  respond_to :html, :js
+
+  # GET /ballistic_calculator
   def new
     @ballistic_calculator = BallisticCalculator.new
-    @calibers = policy_scope(Caliber).all
-    @loads = policy_scope(Load).all
-    @guns = policy_scope(Gun).all
     authorize @ballistic_calculator
   end
 
-  # POST /ballistic_calculators
-  # POST /ballistic_calculators.json
+  # POST /ballistic_calculator
+  # POST /ballistic_calculator.js
   def create
     @ballistic_calculator = BallisticCalculator.new(ballistic_calculator_params)
     authorize @ballistic_calculator
-    @ballistic_calculator.velocity
-    puts @ballistic_calculator.valid?
 
     respond_to do |format|
       if @ballistic_calculator.valid?
+        format.js
         format.html { render :show }
-        format.json { render :show, status: :created, location: @ballistic_calculator }
       else
-        format.html { render :new }
-        format.json { render json: @ballistic_calculator.errors, status: :unprocessable_entity }
+        format.js { render :new }
+        format.html { render :show }
       end
     end
   end
 
   private
+    def set_associations
+      @calibers = policy_scope(Caliber).all
+      @loads = policy_scope(Load).all
+      @guns = policy_scope(Gun).all
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def ballistic_calculator_params
       params.require(:ballistic_calculator).permit(:caliber_id, :load_id, :gun_id,
