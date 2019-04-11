54c335084b3067d393a4204e267b3e02309e209e
Trey Dockendorf <treydock@gmail.com>
2019-04-11 10:15:51 -0400

    When a discarded user signs in their account will be recovered, resolves #39

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,24 @@
 class Users::SessionsController < Devise::SessionsController
   prepend_before_action :check_captcha, only: [:create]
 
+  # POST /users/sign_in
+  def create
+    flash = :signed_in
+    if resource = resource_class.find_for_database_authentication({login: sign_in_params[:login]})
+      if resource.valid_password?(sign_in_params[:password])
+        if resource.discarded?
+          resource.undiscard
+          flash = :signed_in_and_recovered
+        end
+      end
+    end
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, flash)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
+  end
+
   private
     def check_captcha
       unless verify_recaptcha
---
+++ spec/controllers/users_session_controller_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe Users::SessionsController, type: :controller do
+  before(:each) do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+  end
+
+  describe "GET #sign_in" do
+    it "returns a success response" do
+      get :new
+      expect(response).to be_successful
+    end
+  end
+
+  describe "POST #sign_in" do
+    it 'should sign in user' do
+      user = create(:user, email: 'test@example.com', username: 'test', password: 'password', password_confirmation: 'password')
+      post :create, params: {user: {login: 'test', password: 'password'}}
+      expect(response).to redirect_to(root_url)
+      expect(subject.current_user).to eq(user)
+    end
+    it 'should recover and sign in user' do
+      user = create(:user, email: 'test@example.com', username: 'test', password: 'password', password_confirmation: 'password')
+      user.discard
+      post :create, params: {user: {login: 'test', password: 'password'}}
+      user.reload
+      expect(user.discarded?).to be(false)
+      expect(response).to redirect_to(root_url)
+      expect(subject.current_user).to eq(user)
+    end
+    it 'should not sign in user' do
+      user = create(:user, email: 'test@example.com', username: 'test', password: 'password', password_confirmation: 'password')
+      post :create, params: {user: {login: 'test', password: 'password1'}}
+      expect(response).not_to redirect_to(root_url)
+      expect(subject.current_user).not_to eq(user)
+    end
+  end
+end
