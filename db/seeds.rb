c2d75ba3fc653c384ad654894fcab0ce69ac31ac
Trey Dockendorf <treydock@gmail.com>
2019-05-09 15:31:34 -0400

    Seed admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if ! User.find_by(username: 'admin')
+  admin = User.new
+  admin.username = 'admin'
+  admin.email = "admin@#{Settings.domain}"
+  admin.password = Rails.application.credentials.dig(:admin, :password)
+  admin.password_confirmation = Rails.application.credentials.dig(:admin, :password)
+  admin.admin = true
+  admin.skip_confirmation!
+  admin.save!
+end
